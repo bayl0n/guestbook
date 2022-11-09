477557eab3b59c9d7f06f5c9377803061e3f6194
bayl0n <nathanbaylon@gmail.com>
2022-11-10 01:23:26 +1100

    feat(messages): :sparkles: Implement add like feature

--- src/components/ui/Message/MessageItem.tsx
+++ src/components/ui/Message/MessageItem.tsx
@@ -3,6 +3,7 @@ import { HeartIcon as OutlineHeartIcon } from "@heroicons/react/24/outline";
 
 import type { Post, User } from "@prisma/client";
 import type { Session } from "next-auth/core/types";
+import { trpc } from "../../../utils/trpc";
 
 interface Props {
     session: Session | null;
@@ -12,6 +13,12 @@ interface Props {
 }
 
 export default function MessageItem({ session, post, author, likes }: Props) {
+
+    const utils = trpc.useContext();
+    const addLike = trpc.post.addLike.useMutation({
+        onSuccess: () => utils.post.getAll.invalidate(),
+    })
+
     return (
         <div className="flex flex-col gap-2 justify-center text-center mx-auto w-11/12 md:w-1/2 border-2 rounded-md border-neutral-800 p-4">
             <p className="text-neutral-400">
@@ -20,13 +27,26 @@ export default function MessageItem({ session, post, author, likes }: Props) {
             <span className="italic font-bold">- {author.name}</span>
             <span className="text-neutral-500 italic text-sm">{post.createdAt?.toLocaleString('en-US', { dateStyle: "long" })} at {post.createdAt.toLocaleTimeString('en-US')}</span>
             <div className="flex gap-2 justify-center">
-                <button>
-                    {
-                        likes.find(user => {
-                            return (user.name == session?.user?.name);
-                        }) ? <SolidHeartIcon className="w-4 h-4 " /> : <OutlineHeartIcon className="w-4 h-4 " />
-                    }
-                </button>
+                {
+                    likes.find(user => {
+                        return (user.name == session?.user?.name);
+                    }) ?
+                        // Remove like function
+                        <button>
+                            <SolidHeartIcon className="w-4 h-4 " />
+                        </button>
+                        :
+                        // Add like function
+                        <button onClick={() => {
+                            if (!session?.user) return;
+                            addLike.mutate({
+                                postId: post.id,
+                                user: { id: session.user.id }
+                            })
+                        }}>
+                            <OutlineHeartIcon className="w-4 h-4 " />
+                        </button>
+                }
                 <div>
                     {likes.length}
                 </div>
--- src/server/trpc/router/post.ts
+++ src/server/trpc/router/post.ts
@@ -1,27 +1,7 @@
 import { z } from "zod";
-
 import { router, publicProcedure, protectedProcedure } from "../trpc";
 
 export const postRouter = router({
-    postMessage: protectedProcedure
-        .input(
-            z.object({
-                userId: z.string(),
-                message: z.string()
-            })
-        )
-        .mutation(async ({ ctx, input }) => {
-            try {
-                await ctx.prisma.post.create({
-                    data: {
-                        userId: input.userId,
-                        message: input.message
-                    },
-                });
-            } catch (error) {
-                console.log(error);
-            }
-        }),
     getAll: publicProcedure
         .query(async ({ ctx }) => {
             return await ctx.prisma.post.findMany({
@@ -36,6 +16,47 @@ export const postRouter = router({
                 orderBy: {
                     createdAt: "desc",
                 },
+            });
+        }),
+    postMessage: protectedProcedure
+        .input(
+            z.object({
+                userId: z.string(),
+                message: z.string()
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            return await ctx.prisma.post.create({
+                data: {
+                    userId: input.userId,
+                    message: input.message
+                },
+            });
+        }),
+    addLike: protectedProcedure
+        .input(
+            z.object({
+                postId: z.string(),
+                user: z.object({ id: z.string() })
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            const res = await ctx.prisma.post.findUnique({
+                where: { id: input.postId },
+                select: {
+                    likes: true,
+                }
+            });
+
+            if (!res) return { error: "Could not find post" };
+
+            await ctx.prisma.post.update({
+                where: { id: input.postId },
+                data: {
+                    likes: {
+                        set: [...res.likes, input.user],
+                    }
+                }
             });
         }),
 });
